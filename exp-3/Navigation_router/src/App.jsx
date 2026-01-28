6b228a50c9b85c9a981d9bf25b09bda7a72e8c26
Anna <annataneja91@gmail.com>
2026-01-28 13:17:29 +0530

    Added exp-3

--- exp-3/Navigation_router/src/App.jsx
+++ exp-3/Navigation_router/src/App.jsx
@@ -22,7 +22,7 @@ building meaningful, scalable, and user-centric applications.
 function Dashboard(){
   return (
     <>
-    <h1>My Skill Set 🚀</h1>
+    <h1>My Skill Set </h1>
     <h2>Frontend: HTML, CSS, JavaScript, React</h2>
     <h2>Backend: Node.js, Express</h2>
     <h2>Databases: MySQL, MongoDB</h2>
